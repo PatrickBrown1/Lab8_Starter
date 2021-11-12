e6f769400ac9b128c31f3740758523f07c15df8e
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-11-11 17:52:08 -0800

    finished lab

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Lab 8 - Starter
+1) In your own words: Where would you fit your automated tests in your Recipe project development pipeline?
+
+1. Within a Github action that runs whenever code is pushed 
+2. Manually run them locally before pushing code
+3. Run them all after all development is completed
+
+I would include automated tests during all three of these steps, but the most important would be 1, then 2, then 3. Running the tests in a Github action after code is pushed is the most important because it blocks incorrect code from getting pushed into the repo. The second most important would be to have them manually run locally because then developers can make sure their code works locally before committing. Then, the least important is to run them aafter all development is complete as one last sanity check before deployment, but ultimately the code should be correct before this stage.
+
+2) No, we would use unit testing for this.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+No because a messaging feature would include a lot of moving parts including writing, sending receiving, reading, network connections etc, so it would be too complicated to test with unit tests.
+
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+Yes, this would be good for a unit test because it is a simple test that we can do without using the browser and won't affect other tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Phone number tests
+test('valid no area code phone number 1', () => {
+    expect(functions.isPhoneNumber("000-0000")).toBe(true);
+});
+
+test('valid phone number 2', () => {
+    expect(functions.isPhoneNumber("(123) 456-7890")).toBe(true);
+});
+
+test('invalid phone number 2', () => {
+    expect(functions.isPhoneNumber("111111")).toBe(false);
+});
+
+test('invalid phone number 2', () => {
+    expect(functions.isPhoneNumber("1234567890")).toBe(false);
+});
+
+// Email tests
+test('valid email short', () => {
+    expect(functions.isEmail("a@yahoo.com")).toBe(true);
+});
+
+test('valid email long', () => {
+    expect(functions.isEmail("patrickbrown@gmail.com")).toBe(true);
+});
+
+test('invalid email no at', () => {
+    expect(functions.isEmail("abcgmail.com")).toBe(false);
+});
+
+test('invalid email no name', () => {
+    expect(functions.isEmail("@gmail.com")).toBe(false);
+});
+
+// Password tests
+test('strong password 1', () => {
+    expect(functions.isStrongPassword("ABCD")).toBe(true);
+});
+
+test('strong password 2', () => {
+    expect(functions.isStrongPassword("ABCD123")).toBe(true);
+});
+
+test('weak password invalid chars', () => {
+    expect(functions.isStrongPassword("abcabcabc*")).toBe(false);
+});
+
+test('weak password too long', () => {
+    expect(functions.isStrongPassword("abcdefghijk12345as")).toBe(false);
+});
+
+// Date tests
+test('Valid date 1', () => {
+    expect(functions.isDate("1/1/2021")).toBe(true);
+});
+
+test('Valid date full length', () => {
+    expect(functions.isDate("11/11/2021")).toBe(true);
+});
+
+test('invalid date short year', () => {
+    expect(functions.isDate("1/11/11")).toBe(false);
+});
+
+test('invalid date no slash', () => {
+    expect(functions.isDate("12312021")).toBe(false);
+});
+
+// Hex tests
+test('Valid hex short length', () => {
+    expect(functions.isHexColor("#123")).toBe(true);
+});
+
+test('Valid hex full length', () => {
+    expect(functions.isHexColor("#ffffff")).toBe(true);
+});
+
+test('invalid hex special chars', () => {
+    expect(functions.isHexColor("ff*f")).toBe(false);
+});
+
+test('invalid hex 7 chars', () => {
+    expect(functions.isHexColor("#fffffff")).toBe(false);
+});
